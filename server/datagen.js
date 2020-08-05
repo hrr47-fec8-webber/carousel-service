2013b35a0593eb37d33bceaed1ecbadaa637cba2
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-05 12:20:23 -0700

    Configure mySQL connection and import into dataseed file

--- server/datagen.js
+++ server/datagen.js
@@ -1,7 +1,9 @@
 const faker = require('faker');
+const mysql = require('mysql');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const db = require('../db');
 
 
 const downloadImage = (url, imgPath) => {
@@ -19,10 +21,19 @@ const downloadImage = (url, imgPath) => {
     });
 };
 
-axios.get('http://picsum.photos/v2/list')
+/* axios.get('http://picsum.photos/v2/list')
   .then(response => {
     let list = response.data.slice(0, 4);
     return Promise.all(list.map((pic, i) => downloadImage(pic.download_url, path.join(__dirname, `/test/${i}.jpg`))));
   })
   .catch(err => console.log(err));
 
+ */
+
+db.query('DESCRIBE users', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
