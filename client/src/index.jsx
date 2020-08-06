a7b298fc42ea10a771eac6c1207499024e9a94f1
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-06 15:34:49 -0700

    Begin experimenting with react mouseover functionality and install react testing library

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red'
+    };
+    this.hover = this.hover.bind(this);
+  }
+
+  hover() {
+    this.setState({
+      color: this.state.color === 'red' ? 'blue' : 'red'
+    });
+  }
+
+  render() {
+    return (
+      <div style={this.state} onMouseEnter={this.hover} onMouseLeave={this.hover}>What about now?</div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>What About Now?</h1>,
+  <App />,
   document.getElementById('carousel')
 );
