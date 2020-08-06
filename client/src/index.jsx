719feb23577950698e2fa559588bc93d0022662d
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-06 16:33:07 -0700

    Implement successful GET request for image data from the Gallery component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import gallery from './gallery.css';
 
-class GalleryView extends React.Component {
+class Gallery extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'red'
+      color: 'red',
+      images: []
     };
     this.hover = this.hover.bind(this);
+    this.fetch = this.fetch.bind(this);
   }
 
   hover() {
     this.setState({
       color: this.state.color === 'red' ? 'blue' : 'red'
     });
+  }
+
+  fetch(location) {
+    axios.get(`/api/images/${location}`)
+      .then(data => this.setState({images: data.data}));
+  }
+
+  componentDidMount() {
+    this.fetch(this.props.location);
   }
 
   render() {
@@ -24,7 +36,9 @@ class GalleryView extends React.Component {
   }
 }
 
+let location = '1';
+
 ReactDOM.render(
-  <GalleryView />,
+  <Gallery location={location}/>,
   document.getElementById('carousel')
 );
