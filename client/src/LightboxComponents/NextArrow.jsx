5816307a61424371415eb697f0862ce38b52a8ea
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-12 15:51:56 -0700

    SVG elements for forward and back arrows

--- client/src/LightboxComponents/NextArrow.jsx
+++ client/src/LightboxComponents/NextArrow.jsx
@@ -3,7 +3,10 @@ import lightbox from './lightbox.css';
 
 const NextArrow = ({ next, selected, length}) => (
   <div className={selected === length ? lightbox.arrowOff : lightbox.arrow} onClick={next} id="next-arrow">
-    {'>'}
+    <svg height="10" width="6">
+      <line x1="0" y1="0" x2="5" y2="5" style={{ stroke: 'black', strokeWidth: 1.5 }} />
+      <line x1="0" y1="10" x2="5" y2="5" style={{ stroke: 'black', strokeWidth: 1.5 }} />
+    </svg>
   </div>
 );
 
--- client/src/LightboxComponents/PrevArrow.jsx
+++ client/src/LightboxComponents/PrevArrow.jsx
@@ -3,7 +3,10 @@ import lightbox from './lightbox.css';
 
 const PrevArrow = ({ prev, selected }) => (
   <div className={selected === 1 ? lightbox.arrowOff : lightbox.arrow} onClick={prev} id="prev-arrow">
-    {'<'}
+    <svg height="10" width="6" transform="rotate(180)">
+      <line x1="0" y1="0" x2="5" y2="5" style={{ stroke: 'black', strokeWidth: 1.5 }} />
+      <line x1="0" y1="10" x2="5" y2="5" style={{ stroke: 'black', strokeWidth: 1.5 }} />
+    </svg>
   </div>
 );
 
