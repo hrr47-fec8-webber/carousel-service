35f765350ffce3e56cfae5e51a323c69055fe249
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-08-17 16:51:54 -0700

    Update for CORS access

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const db = require('../db');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
 
