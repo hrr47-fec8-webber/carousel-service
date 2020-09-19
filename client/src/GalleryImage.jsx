80524cf93d84779c40e58bf37bf1ff53b9cde477
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-09-19 15:40:45 -0700

    Update CSS to include 3 pic rules

--- client/src/GalleryImage.jsx
+++ client/src/GalleryImage.jsx
@@ -4,7 +4,9 @@ import gallery from './gallery.css';
 
 const GalleryImage = ({ image, length }) => {
   const id = image.img_order;
+  console.log(id)
   const imgClass = `img${id}in${length}`;
+  console.log(imgClass)
   const url = `url(${image.url})`;
   return (
     <div className={gallery[imgClass]} style={{ backgroundImage: url }} name={id} id={id}> </div>
